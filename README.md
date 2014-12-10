4e54ed7d7e8a7ff153a27718e9134ff4386ca19e
Chris Hutchinson <chris_hutchinson@me.com>
2014-12-10 17:27:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# C3 Chart AngularJS Directive
+
+## Version 0.0.1
---
+++ c3chart.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name c3-chart.directive:c3chart
+ * @description
+ * # c3chart
+ */
+angular.module('c3-chart')
+  .directive('c3chart', function ($window) {
+    return {
+      template: '<div></div>',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        scope.$watch(attrs.config, function (v) {
+            if(typeof v !== 'undefined') { 
+                var config = scope[attrs.config];
+                config.bindto = element[0];
+                $window.c3.generate(config);
+            }
+        });
+      }
+    };
+  });
