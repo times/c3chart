38a34208897366d17a53182153a96fe847df9ae5
Ændrew Rininsland <aendrew.rininsland@the-times.co.uk>
2015-04-22 16:51:28 +0100

    Improving support for load/unload, adding gitignore.

--- c3chart.js
+++ c3chart.js
@@ -12,12 +12,46 @@ angular.module('times.c3chart', [])
       template: '<div></div>',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-        scope.$watch(attrs.config, function (v) {
-            if(typeof v !== 'undefined') { 
-                var config = scope[attrs.config];
-                config.bindto = element[0];
-                $window.c3.generate(config);
+        var chart;
+        scope.$watch(attrs.config, function (newValue, oldValue) {
+          if(typeof newValue !== 'undefined') { 
+            var config = newValue;
+            config.bindto = element[0];
+            
+            if (oldValue) { // Chart already exists
+              
+              // Mutate the values to remove all data
+              var copyA = _.cloneDeep(newValue); // Why angular.copy doesn't work here is beyond me.
+              var copyB = _.cloneDeep(oldValue);
+              
+              ['columns', 'rows', 'json'].forEach(function(v){
+                copyA.data[v] = undefined;
+                copyB.data[v] = undefined;
+              });
+
+              // Check whether the two values are data only, or require a full generate()
+              var isData = angular.equals(copyA, copyB);
+              
+              if (isData) { // Only data change; update using chart.load();
+                var dataKeys = Object.keys(newValue.data);
+                var type = dataKeys.indexOf('columns') > -1 ? 'columns' : dataKeys.indexOf('json') > -1 ? 'json' : dataKeys.indexOf('rows') > -1 ? 'rows' : 'undefined';
+                var data = {};
+                data[type] = newValue.data[type];
+                if (typeof(chart) !== 'undefined') {
+                  // chart.unload(); // TODO unload if data names change.
+                  chart.load(data);
+                }
+              } else { // Requires a destroy() and a generate().
+                if (typeof(chart.destroy) !== 'undefined') {
+                  chart.destroy();
+                }
+                
+                chart = $window.c3.generate(config);
+              }
+            } else { // Brand new chart; just generate().
+              chart = $window.c3.generate(config);
             }
+          }
         });
       }
     };
